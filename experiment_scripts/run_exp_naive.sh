cab2586d0f0dbec9f3a529de155b109e8de6ef51
RachelFreedman <rachel.alex.freedman93@gmail.com>
2022-11-17 14:50:00 -0800

    finish and write script for naive baseline

---
+++ experiment_scripts/run_exp_naive.sh
@@ -0,0 +1,9 @@
+julia baselines_naive.jl 3 3 0.9 7 7 5 1000 289506
+julia baselines_naive.jl 3 3 0.9 7 7 5 1000 289513
+julia baselines_naive.jl 3 3 0.9 7 7 5 1000 292312
+julia baselines_naive.jl 3 3 0.9 7 7 5 1000 292319
+
+julia ApproximatePOMDP.jl 3 3 0.9 7 7 5 1000 289506
+julia ApproximatePOMDP.jl 3 3 0.9 7 7 5 1000 289513
+julia ApproximatePOMDP.jl 3 3 0.9 7 7 5 1000 292312
+julia ApproximatePOMDP.jl 3 3 0.9 7 7 5 1000 292319
