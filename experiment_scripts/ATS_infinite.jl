9ece5201f762bd6f9af2e62fa79d8c959de720bf
RachelFreedman <rachel.alex.freedman93@gmail.com>
2023-03-31 15:11:59 -0700

    write ATS infinite-discounted experiment and run scripts

--- experiment_scripts/ATS_infinite.jl
+++ experiment_scripts/ATS_infinite.jl
@@ -1,37 +1,53 @@
-# ARGS = [N, K, discount, u_grain, d_grain, exp_iters, exp_steps, state_index]
+#= 
+Run experiments using active teacher selection on an infinite-horizon, discounted POMDP.
+to run:
+    julia ATS_finite.jl test_boolean num_items num_arms discount utility_granularity arm_granularity num_runs run_length state_index max_depth seed
+=#
 
-using POMDPs, QuickPOMDPs, POMDPModelTools, POMDPPolicies, Parameters, Random, Plots, LinearAlgebra, POMDPTools, BasicPOMCP, D3Trees, GridInterpolations, POMCPOW, POMDPModels, Combinatorics, Dates, Serialization, ParticleFilters
+include("../POMCPOW_modified/src/POMCPOW.jl")
+include("../POMCPOW_modified/src/Solvers.jl")
 
+import .POMCPOW
+import .Solvers
+
+using POMDPs, QuickPOMDPs, POMDPModelTools, POMDPPolicies, Parameters, Random, Plots, LinearAlgebra, POMDPTools, BasicPOMCP, D3Trees, GridInterpolations, POMDPModels, Combinatorics, Dates, Serialization, ParticleFilters
+
+TEST = ARGS[1] == "true"
 expID = Dates.format(Dates.now(), "yymd_HHMMS")
 
 function log(s::String)
-    s_time = Dates.format(Dates.now(), "HH:MM:SS\t")*s*"\n"
-    open("./logs/"*expID*".txt", "a") do file
-        write(file, s_time)
+    if !TEST
+        s_time = Dates.format(Dates.now(), "HH:MM:SS\t")*s*"\n"
+        open("./logs/"*expID*".txt", "a") do file
+            write(file, s_time)
+        end
+        print(s_time)
     end
-    print(s_time)
 end
 
 log("Running experiment with ID "*expID)
 
 @with_kw struct MyParameters
-    N::Int = parse(Int64, ARGS[1])           # size of item set
-    K::Int = parse(Int64, ARGS[2])           # size of arm set
+    N::Int = parse(Int64, ARGS[2])           # size of item set
+    K::Int = parse(Int64, ARGS[3])           # size of arm set
     M::Int = 3                               # size of beta set
-    y::Float64 = parse(Float64, ARGS[3])     # discount factor
+    y::Float64 = parse(Float64, ARGS[4])     # discount factor
     umax::Real = 10                          # max utility
-    u_grain::Int = parse(Int64, ARGS[4])     # granularity of utility approximation
-    d_grain::Int = parse(Int64, ARGS[5])     # granularity of arm distribution approximation
-    beta:: Array{Float64} = [0., 0.01, 50.]     # teacher beta values
-    exp_iters::Int = parse(Int64, ARGS[6])   # number of rollouts to run
-    exp_steps::Int = parse(Int64, ARGS[7])   # number of timesteps per rollout
-    s_index::Int = parse(Int64, ARGS[8])     # index of true state
+    u_grain::Int = parse(Int64, ARGS[5])     # granularity of utility approximation
+    d_grain::Int = parse(Int64, ARGS[6])     # granularity of arm distribution approximation
+    beta:: Array{Float64} = [0., 0.01, 50.]  # teacher beta values
+    exp_iters::Int = parse(Int64, ARGS[7])   # number of rollouts to run
+    exp_steps::Int = parse(Int64, ARGS[8])   # number of timesteps per rollout
+    s_index::Int = parse(Int64, ARGS[9])     # index of true state
+    max_depth::Int = parse(Int64, ARGS[10])  # max depth of search tree
+    seed::Int = parse(Int64, ARGS[11])       # random seed
 end
 
 params = MyParameters()
 log(string(params))
 
 struct State
+    t::Int                    # timesteps remaining before end of run
     u::Array{Float64}         # list of N utility values for N items
     d::Array{Array{Float64}}  # list of K arm distributions, each assigning probabilities to N items
     b::Array{Float64}         # list of M beta values
@@ -85,10 +101,13 @@ B = [params.beta]
 log("generated "*string(length(B))*" beta value sets (each length "*string(length(B[1]))*" teachers)")
 
 # State space
-S = [[State(u,d,b) for u in U, d in D, b in B]...,]
+S = [[State(t,u,d,b) for u in U, d in D, b in B, t in params.exp_steps:-1:1]...,]
 
-log("generated "*string(length(S))*" states")
+# initial states
+S_init = S[1:length(U)*length(D)*length(B)]
 
+log("generated "*string(length(S))*" states, "*string(length(S_init))*" of which are potential start states")
+ 
 # Action space - actions are arm choices (K) or beta selections (M)
 struct Action
     name::String      # valid names are {B,C} + index
@@ -107,8 +126,14 @@ end
 log("generated "*string(length(A))*" actions")
 
 # Transition function
+function next(s::State)
+    # decrement t and preserve rest of state
+    return State(s.t-1, s.u, s.d, s.b)
+end
+
 function T(s::State, a::Action)
-    return SparseCat([s], [1.0])    # categorical distribution
+    s_prime = next(s)
+    return SparseCat([s_prime], [1.0])    # deterministic categorical distribution
 end
 log("generated transition function")
 
@@ -169,7 +194,7 @@ end
 function O(s::State, a::Action, sp::State)
     # if B action, obs in P_obs
     if a.isBeta
-        prob_of_pref = [Pr(o.p, s, s.b[a.index]) for o in P_obs]
+        prob_of_pref = [Pr(o.p, sp, sp.b[a.index]) for o in P_obs]
         prob_of_query = Q
         
         # weight by querying profile to get dist
@@ -178,7 +203,7 @@ function O(s::State, a::Action, sp::State)
         return SparseCat(P_obs, normalized_dist)
     # if C action, obs in I_obs
     else
-        return SparseCat(I_obs, s.d[a.index])
+        return SparseCat(I_obs, sp.d[a.index])
     end
 end
 
@@ -194,26 +219,27 @@ pomdp = QuickPOMDP(MyPOMDP,
     observation  = O,
     reward       = R,
     discount     = params.y,
-    initialstate = S);
+    initialstate = S_init);
 
 log("created POMDP")
 
 # solve POMDP with POMCPOW
-max_depth=5
-solver = POMCPOWSolver(max_depth=max_depth)
+solver = POMCPOW.POMCPOWSolver(max_depth=params.max_depth, rng=Random.seed!(params.seed), estimate_value=RolloutEstimator(Solvers.ConstrainedRandomSolver(actions(pomdp)[1:params.K], Random.seed!(params.seed))))
 planner = solve(solver, pomdp);
-log("solved POMDP using POMCPOW with max search depth "*string(max_depth))
+log("solved POMDP using POMCPOW with max search depth "*string(params.max_depth)* " and rollouts simulated by ConstrainedRandomSolver")
 
-# save serialized planner
-open("./policies/"*expID*"_policy.txt", "w") do file
-    serialize(file, planner)
+if !TEST
+    # save serialized planner
+    open("./policies/"*expID*"_policy.txt", "w") do file
+        serialize(file, planner)
+    end
+    log("saved policy to "*"./policies/"*expID*"_policy.txt")
 end
-
-log("saved policy to "*"./policies/"*expID*"_policy.txt")
 
 # simulate rollouts
 steps = params.exp_steps
 iters = params.exp_iters
+prior = Uniform(S_init)
 
 # POMCPOW rollouts
 # hardcoded initial states
@@ -227,22 +253,23 @@ for iter in 1:iters
     t = 1
     r_accum = 0.
     beliefs_iter = Array{ParticleFilters.ParticleCollection{State}}(undef, steps)
-    for (s, a, o, r, b) in stepthrough(pomdp, planner, updater(planner), Uniform(S), initial_states[iter], "s,a,o,r,b", max_steps=steps)
+    for (s, a, o, r, b) in stepthrough(pomdp, planner, updater(planner), prior, initial_states[iter], "s,a,o,r,b", max_steps=steps, rng=Random.seed!(params.seed))
         r_accum = r_accum + r
         beliefs_iter[t] = b
         if t == 1
             open("./sims/"*expID*"_run"*string(iter)*".txt", "w") do file
                 write(file, string(s))
             end
-#             initial_states[iter] = s
         end
         if a.isBeta
             msg = "\n"*string(t)*",B,"*a.name*",(i"*string(o.p.i0)*"-i"*string(o.p.i1)*";"*string(o.p.label)*"),"*string(r)
         else
             msg = "\n"*string(t)*",C,"*a.name*",i"*string(o.i)*","*string(r)
         end
-        open("./sims/"*expID*"_run"*string(iter)*".txt", "a") do file
-            write(file, msg)
+        if !TEST
+            open("./sims/"*expID*"_run"*string(iter)*".txt", "a") do file
+                write(file, msg)
+            end
         end
         t = t + 1
     end
@@ -251,21 +278,26 @@ for iter in 1:iters
 end
     
 log("ran "*string(iters)*" POMCPOW rollouts for "*string(steps)*" timesteps each")
-log("POMCPOW R: "*string(POMCPOW_R))
 
-open("./beliefs/"*expID*"_belief.txt", "w") do file
-    serialize(file, beliefs)
+if !TEST
+    open("./beliefs/"*expID*"_belief.txt", "w") do file
+        serialize(file, beliefs)
+    end
+    log("saved beliefs to "*"./beliefs/"*expID*"_belief.txt")
 end
-
-log("saved beliefs to "*"./beliefs/"*expID*"_belief.txt")
 
 # calculate maximum possible reward
 max_R = zeros(iters)
+rand_R = zeros(iters)
 
 for iter in 1:iters
     # use the same initial states as the POMCPOW runs
     initial_state = initial_states[iter]
     max_R[iter] = maximum([dot(initial_state.u, initial_state.d[i]) for i in 1:params.K])*steps
+    rand_R[iter] = (mean([dot(initial_state.u, initial_state.d[i]) for i in 1:params.K])*steps)/2.
 end
 
-log("Max R: "*string(max_R))
+log("Max R:\t\t(avg "*string(round(mean(max_R),digits=0))*")\t"*string(max_R))
+log("Random R:\t(avg "*string(round(mean(rand_R),digits=0))*")\t"*string(rand_R))
+log("POMCPOW R:\t(avg "*string(round(mean(POMCPOW_R),digits=0))*")\t"*string(POMCPOW_R))
+log("Normalized R:\t(avg "*string(round(mean(POMCPOW_R./max_R),digits=2))*")\t"*string(POMCPOW_R./max_R))
---
+++ run_scripts/run_ATS_infinite_fast.sh
@@ -0,0 +1,22 @@
+# args: test_boolean num_items num_arms utility_granularity arm_granularity num_runs run_length state_index max_depth seed
+# takes ~45min
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 1217 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 1226 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 1235 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 1245 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 4586 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 4589 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 4629 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 4638 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 4641 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 1572 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 1712 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 3191 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 3204 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 3437 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 3446 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 3514 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 3526 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 3616 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 4151 50 1
+julia ./experiment_scripts/ATS_infinite.jl false 3 3 0.9 3 3 10 100 4426 50 1
