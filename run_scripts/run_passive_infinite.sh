c24bbc6adbd3d7711ad8b16470fe316f68419609
RachelFreedman <rachel.alex.freedman93@gmail.com>
2023-03-31 15:59:14 -0700

    add argument missing from run_passive_infinite(_fast)

--- run_scripts/run_passive_infinite.sh
+++ run_scripts/run_passive_infinite.sh
@@ -1,22 +1,22 @@
 # args: test_boolean num_items num_arms utility_granularity arm_granularity num_runs run_length state_index max_depth seed
 # takes ??
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 1217 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 1226 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 1235 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 1245 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 4586 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 4589 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 4629 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 4638 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 4641 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 1572 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 1712 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 3191 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 3204 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 3437 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 3446 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 3514 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 3526 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 3616 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 4151 100 1
-julia ./experiment_scripts/passive_infinite.jl false 3 3 3 3 25 100 4426 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 1217 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 1226 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 1235 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 1245 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 4586 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 4589 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 4629 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 4638 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 4641 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 1572 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 1712 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 3191 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 3204 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 3437 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 3446 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 3514 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 3526 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 3616 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 4151 100 1
+julia ./experiment_scripts/passive_infinite.jl false 3 3 0.9 3 3 25 100 4426 100 1
